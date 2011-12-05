0a1679a56b0dc1e5d1d964d7f5a4a7ae1a774bfa
Chase Arnold <chase4926@gmail.com>
2011-12-04 20:11:07 -0800

    Added /clear command, add messages to some of the commands, and fixed grammatical errors

--- pEssentials.rb
+++ pEssentials.rb
@@ -4,12 +4,12 @@ class PuruginEssentials
 
   def on_enable
     public_command('feed', 'Nourishes you fully', '/feed ') do |me, *|
-      me.msg green('Your stomach has been filled!')
+      me.msg green('Your stomach has been filled.')
       me.food_level = 30
     end
     
-    public_command('starve', 'Starve yourslef!', '/starve') do |me, *|
-      me.msg red("You are now starving!")
+    public_command('starve', 'Starve yourself!', '/starve') do |me, *|
+      me.msg red('You are now starving!')
       me.food_level = 0
     end
     
@@ -18,6 +18,12 @@ class PuruginEssentials
       player_loc = me.location
       destination = org.bukkit.Location.new(me.world, loc[0], loc[1], loc[2], player_loc.yaw, player_loc.pitch)
       server.scheduler.schedule_sync_delayed_task(self) { me.teleport(destination) }
+      me.msg('You have been teleported to spawn.')
+    end
+    
+    public_command('clear', 'Clears your inventory', '/clear') do |me, *|
+      me.inventory.clear
+      me.msg('Inventory cleared.')
     end
     
     player_command('tpc', 'teleport_to_xyz', '/tpc {x} {y} {z}') do |me, *args|
@@ -32,11 +38,12 @@ class PuruginEssentials
     player_command('setspawn', 'Sets the world\'s spawn', '/setspawn') do |me, *|
       location = me.location.to_a
       me.world.set_spawn_location(location[0], location[1], location[2])
+      me.msg yellow('Spawn changed to your current location')
     end
     
     player_command('heal', 'Heals you fully', '/heal') do |me, *|
-      me.msg "You were fully healed!"
       me.health = 20
+      me.msg green('You have been fully healed.')
     end
     
     player_command('survival', 'Change gamemode to survival.', '/survival') do |me, *|
@@ -50,7 +57,7 @@ class PuruginEssentials
     player_command('mode', 'Change Gamemode to other one.' , '/mode') do |me, *|
       if me.game_mode == org.bukkit.GameMode::CREATIVE then
         me.game_mode = org.bukkit.GameMode::SURVIVAL
-      elsif me.game_mode == org.bukkit.GameMode::SURVIVAL then
+      else
         me.game_mode = org.bukkit.GameMode::CREATIVE
       end
     end
