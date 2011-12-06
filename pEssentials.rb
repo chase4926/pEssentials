5e6793c25d8c2aa2a5b4c4041b21412b8770cf42
Chase Arnold <chase4926@gmail.com>
2011-12-05 21:19:21 -0800

    Added warp commands

--- pEssentials.rb
+++ pEssentials.rb
@@ -1,8 +1,29 @@
 class PuruginEssentials
   include Purugin::Plugin, Purugin::Colors
   description 'PuruginEssentials', 0.2
-
+  
+  def warps_path()
+    @path ||= File.join getDataFolder, 'warps.data'
+  end
+  
+  def save_warps()
+    File.open(warps_path, 'wb') do |io| 
+      Marshal.dump(@warps_hash, io)
+    end
+  end
+  
+  def load_warps()
+    @warps_hash = {}
+    if File.exist?(warps_path) then
+      File.open(warps_path, 'rb') do |io| 
+        @warps_hash = Marshal.load(io)
+      end
+    end
+  end
+  
   def on_enable
+    load_warps()
+    
     public_command('feed', 'Nourishes you fully', '/feed ') do |me, *|
       me.msg green('Your stomach has been filled.')
       me.food_level = 30
@@ -26,12 +47,114 @@ class PuruginEssentials
       me.msg('Inventory cleared.')
     end
     
-    player_command('join', 'Fake join game message', '/join') do |me, *|
-      server.broadcast_message yellow("#{me.display_name} has joined the game")
+    public_command('setwarp', 'Creates a new warp', '/setwarp {warpname} {password}') do |me, *args|
+      if args[0] != nil then
+        location = me.location.to_a
+        location_hash = {}
+        location_hash[:x] = location[0].to_f
+        location_hash[:y] = location[1].to_f
+        location_hash[:z] = location[2].to_f
+        location_hash[:pass] = args[1]
+        @warps_hash[args[0].to_s] = location_hash
+        #File.open(warps_path, 'wb') do |io| 
+          #Marshal.dump(@warps_hash, io)
+        #end
+        save_warps()
+        if args[1] == nil then
+          me.msg green("Warp [#{args[0]}] Set")
+        else
+          me.msg green("Warp [#{args[0]}] Set, With Password #{args[1]}")
+        end
+      else
+        me.msg red('You need to give it a name!')
+      end
     end
     
-    player_command('leave', 'Fake leave game message', '/leave') do |me, *|
-      server.broadcast_message yellow("#{me.display_name} has left the game")
+    public_command('delwarp', 'Deletes a warp', '/delwarp {warpname} {password}') do |me, *args|
+      if args[0] != nil then
+        if @warps_hash[args[0]] != nil then
+          if @warps_hash[args[0]][:pass] == args[1] or @warps_hash[args[0]][:pass] == nil then
+            @warps_hash.delete(args[0])
+            save_warps()
+            me.msg green("Warp [#{args[0]}] deleted")
+          else
+            me.msg red('Incorrect password!')
+          end
+        else
+          me.msg red("Warp [#{args[0]}] does not exist!")
+        end
+      else
+        me.msg red('You need to specify a warp!')
+      end
+    end
+    
+    public_command('warp', 'Warp to a certain location', '/warp {warpname} {password}') do |me, *args|
+      if args[0] != nil then
+        warp = @warps_hash[args[0]]
+        if warp != nil then
+          if warp[:pass] == args[1] or warp[:pass] == nil then
+            player_loc = me.eye_location
+            destination = org.bukkit.Location.new(me.world, warp[:x], warp[:y], warp[:z], player_loc.yaw, player_loc.pitch)
+            server.scheduler.schedule_sync_delayed_task(self) { me.teleport(destination) }
+          else
+            me.msg red('Incorrect password!')
+          end
+        else
+          me.msg red("Warp [#{args[0]}] does not exist!")
+        end
+      else
+        me.msg red('You need to specify a warp!')
+      end
+    end
+    
+    public_command('listwarps', 'Lists the existing warps', '/listwarps') do |me, *|
+      me.msg(@warps_hash.keys.inspect)
+    end
+    
+    player_command('f_warp', 'Force warp', '/f_warp {warpname}') do |me, *args|
+      if args[0] != nil then
+        warp = @warps_hash[args[0]]
+        if warp != nil then
+          player_loc = me.eye_location
+          destination = org.bukkit.Location.new(me.world, warp[:x], warp[:y], warp[:z], player_loc.yaw, player_loc.pitch)
+          server.scheduler.schedule_sync_delayed_task(self) { me.teleport(destination) }
+          me.msg green("Force warped to warp [#{args[0]}]")
+        else
+          me.msg red("Warp [#{args[0]}] does not exist!")
+        end
+      else
+        me.msg red('You need to specify a warp!')
+      end
+    end
+    
+    player_command('f_delwarp', 'Force deletes a warp', '/f_delwarp {warpname}') do |me, *args|
+      if args[0] != nil then
+        if @warps_hash[args[0]] != nil then
+          @warps_hash.delete(args[0])
+          save_warps()
+          me.msg green("Force Deleted Warp [#{args[0]}]")
+        else
+          me.msg red("Warp [#{args[0]}] does not exist!")
+        end
+      else
+        me.msg red('You need to specify a warp!')
+      end
+    end
+    
+    player_command('join', 'Fake join game message', '/join {name}') do |me, *args|
+      if args[0] != nil then
+        server.broadcast_message yellow("#{args[0]} has joined the game")
+      else
+        server.broadcast_message yellow("#{me.display_name} has joined the game")
+      end
+    end
+    
+    player_command('leave', 'Fake leave game message', '/leave {name}') do |me, *args|
+      if args[0] != nil then
+        server.broadcast_message yellow("#{args[0]} has left the game")
+      else
+        server.broadcast_message yellow("#{me.display_name} has left the game")
+      end
     end
     
     player_command('tpc', 'teleport_to_xyz', '/tpc {x} {y} {z}') do |me, *args|
